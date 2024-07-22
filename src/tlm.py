8da068170210bc38140b9300b4af1577503fb18a
Junichi Tsutsui <tsutsui@criepi.denken.or.jp>
2024-07-22 10:08:08 +0900

    Finalized

--- src/tlm.py
+++ src/tlm.py
@@ -1,13 +1,17 @@
 import numpy as np
 import pandas as pd
+from scipy.interpolate import interp1d
 
+from mce.core.forcing import RfAll
 from mce.core.climate import IrmBase
 
 class DriverMCE:
     """
-    Custom MCE driver
+    Custom MCE driver for the two-layer model
+    with or without ocean heat uptake efficacy
     """
     def __init__(self):
+        self.forcing = RfAll()
         self.climate = IrmBase()
         self.parms = {}
 
@@ -18,6 +22,36 @@ class DriverMCE:
         # modified to covert to ZJ unit
         self.ntoa2joule = 4*np.pi * EARTHRADIUS * EARTHRADIUS * SECPERYEAR * 1e-21
 
+        self.cco2_pi_1750 = self.forcing.parms_ar6_ghg.C0_1750
+        self.q4x_ref = self.forcing.c2erf_ar6('CO2', self.cco2_pi_1750 * 4.)
+        self.interp_cco2 = None
+
+    def mk_interp_cco2(self, xlim=(-1., 12.)):
+        """
+        Define an interpolation object to get equivalent CO2 concentrations
+        to well-mixed GHG forcing levels using the AR6 formula of CO2 forcing
+        to relate concentrations to forcing levels.
+
+        Parameters
+        ----------
+        xlim, optional
+            Nominal range of well-mixed GHG forcing in W/m2 across assumed scenarios,
+            by default (-1., 12.)
+        """
+        cco2_2019 = 409.85
+        cn2o_2019 = 332.091
+        erf_co2_2019 = self.forcing.c2erf_ar6('CO2', cco2_2019, cn2o=cn2o_2019)
+
+        # y-value: equivalent CO2 concentrations
+        cco2_pi = self.cco2_pi_1750
+        alpha = erf_co2_2019 / np.log(cco2_2019 / cco2_pi)
+        yp = cco2_pi * np.exp(np.linspace(*xlim, **{'num': 500}) / alpha)
+
+        # x-value: well-mixed GHG forcing levels
+        xp = self.forcing.c2erf_ar6('CO2', yp)
+
+        self.interp_cco2 = interp1d(xp, yp)
+
     def calib(self, parms):
         """Update climate parameters
 
@@ -26,23 +60,32 @@ class DriverMCE:
         parms
             Two-layer model parameters
         """
-        lamg = parms['lamg']
-        gamma_p = parms['gamma_2l']
-        cmix = parms['cmix']
-        cdeep_p = parms['cdeep']
-        eff = parms.get('eff', 1.)
+        p = parms.dropna()
+
+        lamg = p['lamg']
+        gamma_p = p['gamma_2l']
+        cmix = p['cmix']
+        cdeep_p = p['cdeep']
+        eff = p.get('eff', 1.)
 
         if eff != 1.:
-            gamma_p *= eff
-            cdeep_p *= eff
+            gamma_p = gamma_p * eff
+            cdeep_p = cdeep_p * eff
 
         tauj, akj = self.climate.ebm_to_irm([lamg, gamma_p], [cmix, cdeep_p])
         self.climate.parms.update(asj=akj[0], tauj=tauj, lamb=lamg)
 
-        self.parms.update(tauj=tauj, akj=akj, **parms.to_dict())
+        # MCE forcing parameters
+        # q2x = alpha * np.log(2)
+        # q4x = alpha * np.log(4) * beta
+        co2_alpha = p['q2x'] / np.log(2)
+        co2_beta = p.get('co2_beta', 0.5*p['q4x']/p['q2x'])
+        self.forcing.parms.update(alpha=co2_alpha, beta=co2_beta)
+
+        self.parms = {'tauj': tauj, 'akj': akj, **p.to_dict()}
 
     def run(self, qin, tkjlast=None, qlast=None):
-        """Perform time integration
+        """Time integration
 
         Parameters
         ----------
@@ -51,7 +94,7 @@ class DriverMCE:
 
         Returns
         -------
-            Climate response
+            Thermal response
         """
         p = self.parms
         if tkjlast is None:
@@ -73,12 +116,53 @@ class DriverMCE:
 
         ohc = (tlev * np.array([p['cmix'], p['cdeep']])).sum(axis=1) * self.ntoa2joule
 
-        return pd.DataFrame({
+        df = {
             'tg': tlev[1:, 0],
             'ohc': ohc[1:],
             'hflux': hflux[1:],
-        }, index=qin.index)
+        }
+        return pd.DataFrame(df, index=qin.index)
 
+    def run_ideal_co2(self, time, qin):
+        """Time integration for instantaneous or transient forcing changes
+
+        Parameters
+        ----------
+        time
+            Time points, unequal spacing allowed
+        qin
+            Instantaneous forcing change by scalar
+            or transient forcing changes by array
+
+        Returns
+        -------
+            Thermal response
+        """
+        p = self.parms
+
+        if np.isscalar(qin):
+            tlev = (
+                self.climate.response_ideal(time, 'step', 'tres', asj=p['akj'])
+                * (qin / p['lamg'])
+            )
+            hflux = self.climate.response_ideal(time, 'step', 'flux') * qin
+        else:
+            tlev = self.climate.response(time, qin, asj=p['akj'])
+            hflux = qin - p['lamg'] * tlev[:, 0]
+
+        eff = p.get('eff', 1.)
+
+        if eff != 1.:
+            hflux = hflux - (eff - 1.) * p['gamma_2l'] * (tlev[:, 0] - tlev[:, 1])
+
+        ohc = (tlev * np.array([p['cmix'], p['cdeep']])).sum(axis=1) * self.ntoa2joule
+
+        df = {
+            'tg': tlev[:, 0],
+            'ohc': ohc,
+            'hflux': hflux,
+        }
+        return pd.DataFrame(df, index=time)
 
 def ebm_to_irm(df):
     """
